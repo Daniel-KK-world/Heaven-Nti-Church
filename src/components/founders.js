84eac4db03a61b803f0a6aa4a30d894a67c6795e
Daniel P.K <danilofletcher978@gmail.com>
2025-08-21 14:28:39 +0000

    revisions

--- src/components/founders.js
+++ src/components/founders.js
@@ -1,5 +1,6 @@
 import React from "react";
 import pastor4 from "../assets/pastor4.jpg";
+import churchmother from "../assets/churchmother.jpg";
 import "../App.css";
 
 function Founders() {
@@ -26,6 +27,28 @@ function Founders() {
                 <p className="card-text">
                   Founder and Senior Pastor of Heaven Nti Church. A passionate preacher of holiness,
                   deliverance, and the power of the Holy Ghost. Called to raise kingdom champions.
+                </p>
+              </div>
+            </div>
+          </div>          
+        </div>
+
+
+        <div className="row justify-content-center">
+          {/* Pastor */}
+          <div className="col-md-6 col-lg-5 mb-4">
+            <div className="card shadow border-0 founder-card overflow-hidden">
+              <div className="image-container">
+                <img 
+                  src={churchmother} 
+                  alt="Pastor" 
+                  className="card-img-top ken-burns-image" 
+                />
+              </div>
+              <div className="card-body p-4">
+                <h4 className="card-title text-success fw-bold">Prophetess Doris Anane</h4>
+                <p className="card-text">
+                  Assistant Pastor of Heaven Nti Church. A dedicated minister with a strong prophetic gift, passionate about prayer, intercession, and guiding believers into a deeper walk with Christ.
                 </p>
               </div>
             </div>
