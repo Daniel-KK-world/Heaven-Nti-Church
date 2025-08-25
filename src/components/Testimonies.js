d8902c59d729cd21e50f08b17ab7cbc7042704f8
Daniel P.K <danilofletcher978@gmail.com>
2025-08-25 16:29:42 +0000

    testimony text edit

--- src/components/Testimonies.js
+++ src/components/Testimonies.js
@@ -6,7 +6,7 @@ function Testimonies() {
       <section className="hero-cards py-5 bg-light">
         <div className="container text-center">
           <h2 className="fw-bold mb-4 heading-underline">Testimonies of Faith</h2>
-          <p className="lead text-secondary mb-5">
+          <p className="lead mb-5">
             Lives are being transformed by the power of Jesus Christ. Here are just a few stories from members of our community.
           </p>
           <HeroCards />
