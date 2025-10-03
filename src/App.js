33efaabce37045f44f10d9e89775b4adf0e32a22
Daniel P.K <danilofletcher978@gmail.com>
2025-10-03 08:00:40 +0000

    more navbar polish

--- src/App.js
+++ src/App.js
@@ -5,13 +5,14 @@ import Home from "./pages/Home";
 import About from "./pages/About";
 import Ministries from "./pages/Ministries";
 import ContactPage from "./pages/Contact";
+import ScrollToTop from "./components/ScrollToTop";   // ✅ add this
 import "slick-carousel/slick/slick.css"; 
 import "slick-carousel/slick/slick-theme.css";
-
 
 function App() {
   return (
     <Router>
+      <ScrollToTop />   {/* ✅ ensures scroll resets on route change */}
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
