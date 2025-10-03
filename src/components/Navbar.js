d689716ad277e18eaa1f73e9623a4ac424034c79
Daniel P.K <danilofletcher978@gmail.com>
2025-10-03 07:54:11 +0000

    revelator show

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -57,7 +57,7 @@ function Navbar() {
             </ul>
 
             {/* CTA */}
-            <a href="/" className="btn btn-danger text-white px-4 py-2 rounded-pill fw-semibold">
+            <a href="https://youtube.com/@hntvgh157?si=YdWS950jqrHK07qq" className="btn btn-danger text-white px-4 py-2 rounded-pill fw-semibold">
               Revelator Show
             </a>
           </div>
